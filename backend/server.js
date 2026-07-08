b8ab7186e011150b080e15bb186f9c613845eb58
prathamsanidhy-arch <pratham.sanidhy@gmail.com>
2026-07-08 15:29:42 +0530

    bharat

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.get('/api/health', (req, res) => {
 const frontendDistPath = path.join(__dirname, '../frontend/dist');
 app.use(express.static(frontendDistPath));
 
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(frontendDistPath, 'index.html'));
 });
 
